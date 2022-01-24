cac1b51f91c4b0102c1770a238f544ca0e221378
jb <jessb.epicodus@gmail.com>
2022-01-24 10:39:57 -0800

    initial setup; add js; &
    Co-authored by: Eric Crudup <briefcasemanx@gmail.com

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = {}; //array to hold new contact info
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact; 
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1; //auto-adds sequential  ID to contact object
+  return this.currentId;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+// Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
---
+++ readme.md
@@ -0,0 +1,35 @@
+# _Address Book_
+
+#### By _**Jessica Baker & Eric Crudup**_ 
+
+#### _Address Book_
+
+## Technologies Used
+
+* GIT
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* JQuery
+
+## Description
+
+DESCRIBE
+
+## Setup/Installation Requirements
+
+* Go to _https://github.com/jessb-epicodus/address-book.git_
+* Cone this repository to your desktop (or other desired location)
+* Navigate to the top level of the directory called _address-book_
+* Open the index.html file in your browser
+* _This is not yet published_
+
+## Known Bugs
+
+* No known issues
+
+## License
+
+
+Copyright (c) _Jan 2022_ _Jessica Baker & Eric Crudup_
